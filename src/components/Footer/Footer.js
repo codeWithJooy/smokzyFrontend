57ccb6d852e96f9074fb1ca37bd2bc6768d9d94a
Abhishek Hazra <abhishek.hazra@codeclouds.com>
2025-06-13 16:07:33 +0530

    Footer Links Working

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,50 +1,52 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Footer=()=>{
-    return(
-        <div className="footer">
-        <div className="footerUnit">
-          <div className="footerUnitImg">
-            <img src="/assets/common/home.png" />
-          </div>
-          <div className="footerUnitText">
-            <p>Home</p>
-          </div>
+const Footer = () => {
+  const history = useHistory();
+  return (
+    <div className="footer">
+      <div className="footerUnit" onClick={()=>history.push("/dashboard")}>
+        <div className="footerUnitImg">
+          <img src="/assets/common/home.png" />
         </div>
-        <div className="footerUnit">
-          <div className="footerUnitImg">
-            <img src="/assets/common/checkout.png" />
-          </div>
-          <div className="footerUnitText">
-            <p>Orders</p>
-          </div>
-        </div>
-        <div className="footerUnit">
-          <div className="footerUnitImg">
-            <img src="/assets/common/add.png" />
-          </div>
-          <div className="footerUnitText">
-            <p>New</p>
-          </div>
-        </div>
-        <div className="footerUnit">
-          <div className="footerUnitImg">
-            <img src="/assets/common/inventory.png" />
-          </div>
-          <div className="footerUnitText">
-            <p>Inventory</p>
-          </div>
-        </div>
-        <div className="footerUnit">
-          <div className="footerUnitImg">
-            <img src="/assets/common/user.png" />
-          </div>
-          <div className="footerUnitText">
-            <p>Account</p>
-          </div>
+        <div className="footerUnitText">
+          <p>Home</p>
         </div>
       </div>
-    )
-}
+      <div className="footerUnit"  onClick={()=>history.push("/orders")}>
+        <div className="footerUnitImg">
+          <img src="/assets/common/checkout.png" />
+        </div>
+        <div className="footerUnitText">
+          <p>Orders</p>
+        </div>
+      </div>
+      <div className="footerUnit" onClick={()=>history.push("/create")}>
+        <div className="footerUnitImg">
+          <img src="/assets/common/add.png" />
+        </div>
+        <div className="footerUnitText">
+          <p>New</p>
+        </div>
+      </div>
+      <div className="footerUnit">
+        <div className="footerUnitImg">
+          <img src="/assets/common/inventory.png" />
+        </div>
+        <div className="footerUnitText">
+          <p>Inventory</p>
+        </div>
+      </div>
+      <div className="footerUnit">
+        <div className="footerUnitImg">
+          <img src="/assets/common/user.png" />
+        </div>
+        <div className="footerUnitText">
+          <p>Account</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
